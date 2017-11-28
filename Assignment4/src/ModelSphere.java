1383d9c31953c8aabbb30e18808020c98751fbbf
Colin McDaniel <cmcdanie@rams.colostate.edu>
2017-11-28 09:42:15 -0700

    Changed Arrays to RealVector

--- Assignment4/src/ModelSphere.java
+++ Assignment4/src/ModelSphere.java
@@ -1,59 +1,51 @@
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealVector;
 
 public class ModelSphere {
 	
-	public double[] center;
+	public RealVector center;
 	public double r;
 	
-	public double[] ambient;
-	public double[] diffuse;
-	public double[] specular;
-	public double[] attenuation;
+	public RealVector ambient;
+	public RealVector diffuse;
+	public RealVector specular;
+	public RealVector attenuation;
 	
 	public ModelSphere() {
-		center = new double[3];
-		ambient = new double[3];
-		diffuse = new double[3];
-		specular = new double[3];
-		attenuation = new double[3];
+		center = new ArrayRealVector(3);
+		ambient = new ArrayRealVector(3);
+		diffuse = new ArrayRealVector(3);
+		specular = new ArrayRealVector(3);
+		attenuation = new ArrayRealVector(3);
 		//setCenter(cx, cy, cz);
 		//r = radius;
 	}
 	
-	public void setCenter(double cx, double cy, double cz) {
-		center[0] = cx;
-		center[1] = cy;
-		center[2] = cz;
+	public void setCenter(RealVector cntr) {
+		center = cntr;
 	}
 	
 	public void setRadius(double radius) {
 		r = radius;
 	}
 	
-	public void setAmbient(double Kar, double Kag, double Kab) {
-		ambient[0] = Kar;
-		ambient[1] = Kag;
-		ambient[2] = Kab;
+	public void setAmbient(RealVector Ka) {
+		ambient = Ka;
 	}
 	
-	public void setDiffuse(double Kdr, double Kdg, double Kdb) {
-		diffuse[0] = Kdr;
-		diffuse[1] = Kdg;
-		diffuse[2] = Kdb;
+	public void setDiffuse(RealVector Kd) {
+		diffuse = Kd;
 	}
 	
-	public void setSpecular(double Ksr, double Ksg, double Ksb) {
-		specular[0] = Ksr;
-		specular[1] = Ksg;
-		specular[2] = Ksb;
+	public void setSpecular(RealVector Ks) {
+		specular = Ks;
 	}
 	
-	public void setAttenuation(double Krr, double Krg, double Krb) {
-		attenuation[0] = Krr;
-		attenuation[1] = Krg;
-		attenuation[2] = Krb;
+	public void setAttenuation(RealVector Kr) {
+		attenuation = Kr;
 	}
 	
-	public double[] getCenter() {
+	public RealVector getCenter() {
 		return center;
 	}
 	
@@ -61,19 +53,19 @@ public class ModelSphere {
 		return r;
 	}
 	
-	public double[] getAmbient() {
+	public RealVector getAmbient() {
 		return ambient;
 	}
 	
-	public double[] getDiffuse() {
+	public RealVector getDiffuse() {
 		return diffuse;
 	}
 	
-	public double[] getSpecular() {
+	public RealVector getSpecular() {
 		return specular;
 	}
 	
-	public double[] getAttenuation() {
+	public RealVector getAttenuation() {
 		return attenuation;
 	}
 }
